35fb4e072191d1fb24ae5e318c4f2b0ddd324810
dev.rayavaram <dev.rayavaram@gmail.com>
2020-05-29 13:40:39 -0500

    added routr funtion to app

--- art-store-client/src/App.js
+++ art-store-client/src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import 'normalize.css'; // Note this
+import 'normalize.css'; // Note this;
+import Home from './components/Home';
+import Contact from './components/Contact'
+import Order from './components/Order';
+import Product from './components/Products'
+import { BrowserRouter as Router, Route,Switch,  Link} from 'react-router-dom';
+import Menubar from './Navbar'
+
 
 import './App.scss'
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World! 
-      <span> GATEWAY TO INDIAN ART</span>
-      </h1>
+      <h1><button tag={Link} to="/Home">Enter</button>
+      <span> GATEWAY TO INDIAN ART</span>      </h1>
+
+      <Router>
+        <Menubar/>        
+        <Switch>
+          <Route exact path='/product' component={Product}/>
+          <Route exact path='/order/:id' component={Order}/>
+          <Route exact path='/contact' component={Contact}/>
+          <Route exact path='/'  component={Home}/>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
