ae8c38e2c159cff7402ae44b06840895841d2c23
dev.rayavaram <dev.rayavaram@gmail.com>
2020-05-29 01:00:10 -0500

    added stylesheet

--- art-store-client/src/App.js
+++ art-store-client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import 'normalize.css'; // Note this
+
 import './App.scss'
 function App() {
   return (
     <div className="App">
-      
+      <h1>Hello, World! 
+      <span> GATEWAY TO INDIAN ART</span>
+      </h1>
     </div>
   );
 }
