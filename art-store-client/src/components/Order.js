3080f2398d4e2481de14c0e455256b4ec71e04c9
dev.rayavaram <dev.rayavaram@gmail.com>
2020-05-29 19:02:55 -0500

    image files added and created basic styles

--- art-store-client/src/components/Order.js
+++ art-store-client/src/components/Order.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Order() {
     return (
-    <>
+    <div className='order'>
             <h3 className="title">Order page</h3>
-      </>
+      </div>
 
     );
 };
--- art-store-client/src/components/Products.js
+++ art-store-client/src/components/Products.js
@@ -2,9 +2,37 @@ import React from 'react';
 
 function Product() {
     return (
-    <>
-            <h3 className="title">Products page</h3>
-      </>
+    <div className='product'>
+            <h1>Paintings</h1>
+            <div className="container-1">
+                <img  src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
+            </div>
+            <div className="container-2">
+            <img src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
+
+            </div>
+            <div className="container-3">
+            <img src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
+            </div>
+            <div className="container-4">
+            <img src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
+            </div>
+            <div className="container-5">
+            <img src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
+            </div>
+            <div className="container-6">
+            <img src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
+            </div>
+            <div className="container-7">
+            <img src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
+            </div>
+            <div className="container-8">
+            <img src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
+            </div>
+            <div className="container-9">
+            <img src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
+            </div>
+      </div>
 
     );
 };
