8c2ad7eecc026f316dbbb3cd93946afcdc3440cc
dev.rayavaram <dev.rayavaram@gmail.com>
2020-05-29 22:23:31 -0500

    added grids

--- art-store-client/src/components/Products.js
+++ art-store-client/src/components/Products.js
@@ -1,40 +1,121 @@
-import React from 'react';
-
-function Product() {
+import React,{Component} from 'react';
+import { Button } from 'react-bootstrap';
+ 
+class Product extends Component {
+  render(){
     return (
-    <div className='product'>
-            <h1>Paintings</h1>
-            <div className="container-1">
-                <img  src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
+      <div className='product'>
+            <div className="banner">
+                <span> GATEWAY TO INDIAN ART</span> 
             </div>
-            <div className="container-2">
-            <img src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
+          <section id="projects">
+              <ul>
+                <li>
+                  <img  src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
+                  <div className="product-details">
+                  <p>
+                   Name:
+                    Price:
+                    Quantity:
+                     </p> 
+                    <Button className="button" type="button" value="button"  onClick={this.addOrder}>Buy</Button>
+                  </div>
+                </li>
+                <li>
+                  <img  src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
+                  <div className="product-details">
+                  <p>
+                   Name:
+                    Price:
+                    Quantity:
+                     </p> 
+                    <Button color="success" type="button" value="button"  onClick={this.addOrder}>Buy</Button>
+                  </div>
+                </li>
+                <li>
+                  <img  src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
+                  <div className="product-details">
+                  <p>
+                   Name:
+                    Price:
+                    Quantity:
+                     </p> 
+                    <Button color="success" type="button" value="button"  onClick={this.addOrder}>Buy</Button>
+                  </div>
+                </li>
+                <li>
+                  <img  src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
+                  <div className="product-details">
+                  <p>
+                   Name:
+                    Price:
+                    Quantity:
+                     </p> 
+                    <Button color="success" type="button" value="button"  onClick={this.addOrder}>Buy</Button>
+                  </div>
+                </li>
+                <li>
+                  <img  src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
+                  <div className="product-details">
+                  <p>
+                   Name:
+                    Price:
+                    Quantity:
+                     </p> 
+                    <Button color="success" type="button" value="button"  onClick={this.addOrder}>Buy</Button>
+                  </div>
+                </li>
+                <li>
+                  <img  src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
+                  <div className="product-details">
+                  <p>
+                   Name:
+                    Price:
+                    Quantity:
+                     </p> 
+                    <Button color="success" type="button" value="button"  onClick={this.addOrder}>Buy</Button>
+                  </div>
+                </li>
+                <li>
+                  <img  src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
+                  <div className="product-details">
+                  <p>
+                   Name:
+                    Price:
+                    Quantity:
+                     </p> 
+                    <Button color="success" type="button" value="button"  onClick={this.addOrder}>Buy</Button>
+                  </div>
+                </li>
+                <li>
+                  <img  src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
+                  <div className="product-details">
+                  <p>
+                   Name:
+                    Price:
+                    Quantity:
+                     </p> 
+                    <Button color="success" type="button" value="button"  onClick={this.addOrder}>Buy</Button>
+                  </div>
+                </li>
+                <li>
+                  <img  src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
+                  <div className="product-details">
+                  <p>
+                   Name:
+                    Price:
+                    Quantity:
+                     </p> 
+                    <Button color="success" type="button" value="button"  onClick={this.addOrder}>Buy</Button>
+                  </div>
+                </li>
 
+              </ul>
+            </section>
             </div>
-            <div className="container-3">
-            <img src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
-            </div>
-            <div className="container-4">
-            <img src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
-            </div>
-            <div className="container-5">
-            <img src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
-            </div>
-            <div className="container-6">
-            <img src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
-            </div>
-            <div className="container-7">
-            <img src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
-            </div>
-            <div className="container-8">
-            <img src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
-            </div>
-            <div className="container-9">
-            <img src={require('../images/image-sm-1.jpg')}  alt="tanjore"/>
-            </div>
-      </div>
-
-    );
+      );
+  }
+  
 };
     
 export default Product;
